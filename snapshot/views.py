3a797dd0af10c4161d3e6221315176f31147fb78
Cayo Puigdefabregas <cayo@puigdefabregas.eu>
2024-07-13 15:27:26 +0200

    upload form

--- snapshot/views.py
+++ snapshot/views.py
@@ -1,7 +1,11 @@
 from django.utils.translation import gettext_lazy as _
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
+
 from dashboard.mixins import  DashboardView
 from snapshot.models import Snapshot
+from snapshot.forms import UploadForm
 # from django.shortcuts import render
 # from rest_framework import viewsets
 # from snapshot.serializers import SnapshotSerializer
@@ -25,3 +29,12 @@ class ListSnapshotsView(DashboardView, TemplateView):
             'snapshots': snapshots,
         })
         return context
+
+
+class UploadView(DashboardView, FormView):
+    template_name = "upload.html"
+    section = "snapshots"
+    title = _("Upload Snapshot")
+    breadcrumb = "Snapshots / Upload"
+    success_url = reverse_lazy('snashot:list')
+    form_class = UploadForm
