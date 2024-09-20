1834aab36ecea6d88ca9e85f445d727a483f1e92
pedro <git2021@cas.cat>
2024-09-20 12:19:12 -0300

    add docker-reset
    with the usual operations to restart everything

---
+++ docker-reset.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Copyright (c) 2024 Pedro <copyright@cas.cat>
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+set -e
+set -u
+# DEBUG
+#set -x
+
+main() {
+        # remove old database
+        sudo rm -v db/*
+        docker compose down
+        docker compose build
+        docker compose up
+}
+
+main "${@}"
+
+# written in emacs
+# -*- mode: shell-script; -*-
