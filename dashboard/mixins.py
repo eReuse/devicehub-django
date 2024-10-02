aa31aefb4235c5678bfc299ce199c347f6829bd1
Cayo Puigdefabregas <cayo@puigdefabregas.eu>
2024-10-02 12:13:05 +0200

    Pagination for list of devices in dashboard and lots

--- dashboard/mixins.py
+++ dashboard/mixins.py
@@ -1,5 +1,5 @@
 from django.urls import resolve
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect, Http404
 from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import PermissionDenied
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -44,7 +44,6 @@ class DashboardView(LoginRequiredMixin):
         return context
 
     def get_session_devices(self):
-        # import pdb; pdb.set_trace()
         dev_ids = self.request.session.pop("devices", [])
         
         self._devices = []
@@ -82,3 +81,28 @@ class InventaryMixin(DashboardView, TemplateView):
             except Exception:
                 pass
         return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        limit = self.request.GET.get("limit", 10)
+        page = self.request.GET.get("page", 1)
+        if limit:
+            try:
+                limit = int(limit)
+                page = int(page)
+            except:
+                raise Http404
+
+        offset = (page - 1) * limit
+        devices, count = self.get_devices(self.request.user, offset, limit)
+        total_pages = (count + limit - 1) // limit
+
+        context.update({
+            'devices': devices,
+            'count': count,
+            "limit": limit,
+            "offset": offset,
+            "page": page,
+            "total_pages": total_pages,
+        })
+        return context
---
+++ dashboard/templatetags/paginacion.py
@@ -0,0 +1,19 @@
+from django import template
+
+register = template.Library()
+
+@register.inclusion_tag('pagination.html')
+def render_pagination(page_number, total_pages):
+    """
+    Template tag for render pagination
+
+    Args:
+    - page_number: number of actual page
+    - total_pages: total pages.
+
+    Use it template: {% render_pagination page_number total_pages %}
+    """
+    return {
+        'page_number': page_number,
+        'total_pages': total_pages,
+    }
