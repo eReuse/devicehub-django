98629728f7933a2c25aabd6fe9414b89eaacf980
Cayo Puigdefabregas <cayo@puigdefabregas.eu>
2024-10-07 16:57:26 +0200

    add admin from add_user command

--- user/management/commands/add_user.py
+++ user/management/commands/add_user.py
@@ -14,19 +14,22 @@ class Command(BaseCommand):
         parser.add_argument('institution', type=str, help='institution')
         parser.add_argument('email', type=str, help='email')
         parser.add_argument('password', type=str, help='password')
+        parser.add_argument('is_admin', nargs='?', default=False, type=str, help='is admin')
 
     def handle(self, *args, **kwargs):
         email = kwargs['email']
         password = kwargs['password']
+        is_admin = kwargs['is_admin']
         institution = Institution.objects.get(name=kwargs['institution'])
-        self.create_user(institution, email, password)
+        self.create_user(institution, email, password, is_admin)
         self.create_lot_tags()
 
-    def create_user(self, institution, email, password):
+    def create_user(self, institution, email, password, is_admin):
         self.u = User.objects.create(
             institution=institution,
             email=email,
-            password=password
+            password=password,
+            is_admin=is_admin,
         )
         self.u.set_password(password)
         self.u.save()
