dbff6ec60652fd5fe3df863d056ee522b771d2c0
Cayo Puigdefabregas <cayo@puigdefabregas.eu>
2024-10-07 16:56:24 +0200

    edit and delete user

--- admin/urls.py
+++ admin/urls.py
@@ -6,4 +6,7 @@ app_name = 'admin'
 urlpatterns = [
     path("panel/", views.PanelView.as_view(), name="panel"),
     path("users/", views.UsersView.as_view(), name="users"),
+    path("users/new", views.CreateUserView.as_view(), name="new_user"),
+    path("users/edit/<int:pk>", views.EditUserView.as_view(), name="edit_user"),
+    path("users/delete/<int:pk>", views.DeleteUserView.as_view(), name="delete_user"),
 ]
