f74db30627421090465eef5d82ca8ebfcf1d76a0
Cayo Puigdefabregas <cayo@puigdefabregas.eu>
2024-10-14 17:43:32 +0200

    change response from api/snapshots

--- device/models.py
+++ device/models.py
@@ -27,7 +27,7 @@ class Device:
         # the id is the chid of the device
         self.id = kwargs["id"]
         self.pk = self.id
-        self.shortid = self.pk[:6]
+        self.shortid = self.pk[:6].upper()
         self.algorithm = None
         self.owner = None
         self.annotations =  []
