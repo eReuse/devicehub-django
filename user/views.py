a8a29cd25d82f02171fee3981e91b09c711de4c1
Cayo Puigdefabregas <cayo@puigdefabregas.eu>
2024-10-14 11:34:52 +0200

    we don't need for the moment edit profile of user

--- user/views.py
+++ user/views.py
@@ -1,5 +1,5 @@
 from decouple import config
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils.translation import gettext_lazy as _
@@ -47,35 +47,4 @@ class SettingsView(DashboardView, FormView):
         kwargs = super().get_form_kwargs()
         kwargs['tokens'] = tokens
         return kwargs
-
-class ProfileView(DashboardView):
-    template_name = "profile.html"
-    subtitle = _('My personal data')
-    icon = 'bi bi-person-gear'
-    fields = ('first_name', 'last_name', 'email')
-    success_url = reverse_lazy('idhub:user_profile')
-    model = User
-
-    def get_queryset(self, **kwargs):
-        queryset = Membership.objects.select_related('user').filter(
-                user=self.request.user)
-
-        return queryset
-
-    def get_object(self):
-        return self.request.user
-
-    def get_form(self):
-        form = super().get_form()
-        return form
-
-    def form_valid(self, form):
-        return super().form_valid(form)
-        
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update({
-            'lang': self.request.LANGUAGE_CODE,
-        })
-        return context
 
