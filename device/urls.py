1b6af509e1fd2209fbd2c80024a16d2807f902b4
thomas rusiecki <rusieckithomas@gmail.com>
2024-11-12 11:47:32 -0300

    annotations renaming on views

--- device/urls.py
+++ device/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("add/", views.NewDeviceView.as_view(), name="add"),
     path("edit/<str:pk>/", views.EditDeviceView.as_view(), name="edit"),
     path("<str:pk>/", views.DetailsView.as_view(), name="details"),
-    path("<str:pk>/annotation/add", views.AddAnnotationView.as_view(), name="add_annotation"),
+    path("<str:pk>/user_property/add", views.AddUserPropertyView.as_view(), name="add_user_property"),
     path("<str:pk>/document/add", views.AddDocumentView.as_view(), name="add_document"),
     path("<str:pk>/public/", views.PublicDeviceWebView.as_view(), name="device_web"),
 
