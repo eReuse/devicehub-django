b759c53e750877e15c7ae0b2b337052e87d3608f
Cayo Puigdefabregas <cayo@puigdefabregas.eu>
2024-11-20 10:52:22 +0100

    add memberFederated model

--- dpp/models.py
+++ dpp/models.py
@@ -20,7 +20,7 @@ class MemberFederated(models.Model):
     domain = models.CharField(max_length=STR_EXTEND_SIZE)
     # This client_id and client_secret is used for connected to this domain as
     # a client and this domain then is the server of auth
-    client_id = models.CharField(max_length=STR_EXTEND_SIZE. null=True)
+    client_id = models.CharField(max_length=STR_EXTEND_SIZE, null=True)
     client_secret = models.CharField(max_length=STR_EXTEND_SIZE, null=True)
     institution = models.ForeignKey(
         Institution, on_delete=models.SET_NULL, null=True, blank=True)
