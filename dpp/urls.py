ddb764b3ead3cde31bcb2ff4eea4bb40e1948e07
Cayo Puigdefabregas <cayo@puigdefabregas.eu>
2024-11-28 17:25:40 +0100

    dpp for proofs

--- dpp/urls.py
+++ dpp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from dpp import views 
 
-app_name = 'proofs'
+app_name = 'dpp'
 
 urlpatterns = [
     path("<int:proof_id>/", views.ProofView.as_view(), name="proof"),
