e0d9e9fe308954827689d5cb4352602e92852433
Thomas Rusiecki <rusieckithomas@gmail.com>
2024-11-30 04:38:19 -0300

    initial orm state models

--- action/models.py
+++ action/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+from django.db import models, connection
+from user.models import User, Institution
 
-# Create your models here.
+
+class StateDefinition(models.Model):
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    order = models.PositiveIntegerField()
+    state = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.institution.name} - {self.state}"
+
+class State(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    state = models.CharField(max_length=255)
+    snapshot_uuid = models.UUIDField()
+
+    def __str__(self):
+        return f"{self.institution.name} - {self.state} - {self.snapshot_uuid}"
