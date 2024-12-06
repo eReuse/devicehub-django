9a9970336b22ddff9ec289742f3920dfe91a5679
Thomas Rusiecki <rusieckithomas@gmail.com>
2024-12-05 23:09:17 -0300

    added view and url for new action

--- action/urls.py
+++ action/urls.py
@@ -1,1 +1,10 @@
 from django.urls import path, include
+from action import views
+
+app_name = 'action'
+
+urlpatterns = [
+
+    path("new/", views.NewActionView.as_view(), name="new_action"),
+
+]
