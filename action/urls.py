12711c2d5ff7c66bfdc0abbdbe5d2b0b30b05e3b
Thomas Rusiecki <rusieckithomas@gmail.com>
2024-12-19 18:17:09 -0300

    simpler state change and action input

--- action/urls.py
+++ action/urls.py
@@ -5,7 +5,8 @@ app_name = 'action'
 
 urlpatterns = [
 
-    path("new/", views.NewActionView.as_view(), name="new_action"),
-    path('state/<int:pk>/undo/', views.ActionUndoView.as_view(), name='undo_action'),
+    path("new/", views.ChangeStateView.as_view(), name="change_state"),
+    path('state/<int:pk>/undo/', views.UndoStateView.as_view(), name='undo_state'),
+    path('note/add/', views.AddNoteView.as_view(), name='add_note'),
 
 ]
