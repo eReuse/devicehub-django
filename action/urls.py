988c04f3be73113524dec8eb748bc7f4f927000c
Thomas Rusiecki <rusieckithomas@gmail.com>
2025-01-04 01:54:20 -0300

    adding remove button for notes

--- action/urls.py
+++ action/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("new/", views.ChangeStateView.as_view(), name="change_state"),
     path('note/add/', views.AddNoteView.as_view(), name='add_note'),
     path('note/edit/<int:pk>', views.UpdateNoteView.as_view(), name='update_note'),
+    path('note/delete/<int:pk>', views.DeleteNoteView.as_view(), name='delete_note'),
 ]
