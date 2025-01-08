3cedece3a4f1d7aa27c1a3ad226f12716dd30065
Thomas Rusiecki <rusieckithomas@gmail.com>
2025-01-08 17:11:33 -0300

    added logging for evidence tag changes

--- evidence/urls.py
+++ evidence/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path("<uuid:pk>", views.EvidenceView.as_view(), name="details"),
     path("<uuid:pk>/eraseserver", views.EraseServerView.as_view(), name="erase_server"),
     path("<uuid:pk>/download", views.DownloadEvidenceView.as_view(), name="download"),
+    path("tag/<str:pk>/delete", views.DeleteEvidenceTagView.as_view(), name="delete_tag"),
 ]
