d7d6fb7bc6d68242e3369903aa649910c6c8ae32
pedro <git2021@cas.cat>
2025-01-30 08:39:22 +0100

    make devicehub autosign a vc using idhub

--- docker-reset.sh
+++ docker-reset.sh
@@ -31,6 +31,7 @@ main() {
         # deactivate configured flag
         rm -vfr ./already_configured
         docker compose down -v
+        docker compose pull --ignore-buildable
         if [ "${DEV_DOCKER_ALWAYS_BUILD:-}" = 'true' ]; then
                 docker compose build
         fi
