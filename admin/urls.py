5b5b864f80e1d59a0e25f65e57a46fdd5f3e0a2a
Cayo Puigdefabregas <cayo@puigdefabregas.eu>
2025-02-03 13:37:44 +0100

    admin lot tags

--- admin/urls.py
+++ admin/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path("users/edit/<int:pk>", views.EditUserView.as_view(), name="edit_user"),
     path("users/delete/<int:pk>", views.DeleteUserView.as_view(), name="delete_user"),
     path("institution/<int:pk>", views.InstitutionView.as_view(), name="institution"),
+    path("lot_tag/", views.LotTagPanelView.as_view(), name="tag_panel"),
+    path("lot_tag/add", views.AddLotTagView.as_view(), name="add_lot_tag"),
+    path("lot_tag/delete/<int:pk>", views.DeleteLotTagView.as_view(), name='delete_lot_tag'),
+    path("lot_tag/edit/<int:pk>/", views.UpdateLotTagView.as_view(), name='edit_lot_tag'),
 ]
