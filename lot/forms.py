5bf873f2823a448ec36668e029fdaba7f7706ba4
Thomas Rusiecki <rusieckithomas@gmail.com>
2025-03-03 13:08:52 -0300

    add collapse and search function

--- lot/forms.py
+++ lot/forms.py
@@ -4,9 +4,20 @@ from lot.models import Lot
 
 class LotsForm(forms.Form):
     lots = forms.ModelMultipleChoiceField(
-        queryset=Lot.objects.all(),
+        queryset=Lot.objects.filter(archived=False),
         widget=forms.CheckboxSelectMultiple,
     )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        #grouping lots by their lot group for more readability
+        self.grouped_lots = {}
+        for lot in self.fields['lots'].queryset:
+            group_name = lot.type.name if lot.type else "No group"
+            if group_name not in self.grouped_lots:
+                self.grouped_lots[group_name] = []
+            self.grouped_lots[group_name].append(lot)
 
     def clean(self):
         self._lots = self.cleaned_data.get("lots")
