8dbea29dcd13dfa4cbdd0e4f078dcc0c009325c5
Thomas Rusiecki <rusieckithomas@gmail.com>
2025-03-06 17:53:00 -0300

    added django table and delete summary

--- lot/views.py
+++ lot/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404, redirect, Http404, render
 from django.contrib import messages
 from django.core.cache import cache
 from django.utils.translation import gettext_lazy as _
+from django.utils.safestring import mark_safe
 from django.db.models import Q, Count, Case, When, IntegerField
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import (
