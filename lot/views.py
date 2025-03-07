fc2154405e313b904d782a3842c56059f66d9f01
Cayo Puigdefabregas <cayo@puigdefabregas.eu>
2025-03-07 16:54:35 +0100

    add subscription and donor correctly

--- lot/views.py
+++ lot/views.py
@@ -450,6 +450,9 @@ class SubscriptLotView(DashboardView, FormView):
             id=self.pk
         )
 
+
+class SubscriptLotView(SubscriptLotMixing):
+
     def form_valid(self, form):
         form.save()
         response = super().form_valid(form)
