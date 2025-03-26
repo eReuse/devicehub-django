c8324da77c03ff44a3957c41414c76a96310b288
Thomas Rusiecki <rusieckithomas@gmail.com>
2025-03-26 17:28:32 -0300

    sort by date

--- lot/views.py
+++ lot/views.py
@@ -105,7 +105,7 @@ class AddToLotView(DashboardView, FormView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        lots = Lot.objects.filter(owner=self.request.user.institution)
+        lots = Lot.objects.filter(owner=self.request.user.institution).order_by('-created')
         lot_tags = LotTag.objects.filter(owner=self.request.user.institution)
         context.update({
             'lots': lots,
