672ab93b8c44d0e0405785ed671f580cfb32e55a
sergio-gimenez <hi@sergiogimenez.com>
2025-03-26 11:26:44 +0100

    Do not use data class and make object generic

--- environmental_impact/models.py
+++ environmental_impact/models.py
@@ -1,9 +1,5 @@
-from dataclasses import dataclass
-from django.db import models
-
-
-@dataclass
 class EnvironmentalImpact:
-    carbon_saved: float = 0.0
-    co2_emissions: float = 0.0
+    co2_emissions: dict = {}
+    relevant_input_data: dict = {}
+    constants: dict = {}
     docs: str = ""
