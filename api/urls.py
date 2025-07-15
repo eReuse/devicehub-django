6829a50db9fa091affc76b9faa105f2d2baa79cc
Thomas Rusiecki <rusieckithomas@gmail.com>
2025-07-15 14:34:07 -0300

    url configs now done by ninja

--- api/urls.py
+++ api/urls.py
@@ -1,15 +1,1 @@
 #see dhub/urls.py
-
-from api import views
-
-from django.urls import path
-
-
-app_name = 'api'
-
-urlpatterns = [
-    path('v1/snapshot/', views.NewSnapshotView.as_view(), name='new_snapshot'),
-    path('v1/property/<str:pk>/', views.AddPropertyView.as_view(), name='new_property'),
-    path('v1/device/<str:pk>/', views.DetailsDeviceView.as_view(), name='device'),
-
-]
