2a0c0400ae7512a76575db27ad8c434a03f20458
Thomas Rusiecki <rusieckithomas@gmail.com>
2025-07-15 14:34:07 -0300

    url configs now done by ninja

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -5,5 +5,5 @@ from api.auth import GlobalAuth
 
 api = NinjaAPI(auth= GlobalAuth() ,version='1.0.0')
 api.add_router("/lots", lots.router, tags=["Lots"])
-api.add_router("/snapshot/", snapshot.router, tags=["Devices"])
+api.add_router("/snapshot/", snapshot.router, tags=["Snapshots"])
 api.add_router("/devices/", devices.router, tags=["Devices"])
