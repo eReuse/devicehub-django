5858dc41cabd35e6225fb197cd5072dece13f9ca
Thomas Rusiecki <rusieckithomas@gmail.com>
2025-07-26 03:21:51 -0300

    minor changes
    updated template for django-tables
    
    same table template for multiple tables

--- lot/tables.py
+++ lot/tables.py
@@ -14,8 +14,6 @@ class LotTable(tables.Table):
             'td__input': {
                 'class': 'select-checkbox form-check-input'
             },
-            'th': {'class': 'text-center'},
-            'td': {'class': 'text-center'}
         },
         orderable=False
     )
@@ -23,56 +21,33 @@ class LotTable(tables.Table):
         linkify=("dashboard:lot", {"pk": tables.A("id")}),
         verbose_name=_("Lot Name"),
         attrs={
-            'th': {'class': 'text-start'},
-            'td': {'class': 'fw-bold text-start'}
         }
     )
     description = tables.Column(
         verbose_name=_("Description"),
         default=_("No description"),
         attrs={
-            'th': {'class': 'text-start'},
-            'td': {'class': 'text-muted text-start'}
+            'td': {'class': 'text-muted'}
         }
     )
     archived = tables.Column(
         verbose_name=_("Status"),
-        attrs={
-            'th': {'class': 'text-center'},
-            'td': {'class': 'text-center'}
-        }
     )
     device_count = tables.Column(
         verbose_name=_("Devices"),
         accessor='device_count',
-        attrs={
-            'th': {'class': 'text-center'},
-            'td': {'class': 'text-center'}
-        }
     )
     created = tables.DateColumn(
         format="Y-m-d",
         verbose_name=_("Created On"),
-        attrs={
-            'th': {'class': 'text-end'},
-            'td': {'class': 'text-end'}
-        }
     )
     user = tables.Column(
         verbose_name=_("Created By"),
         default=_("Unknown"),
-        attrs={
-            'th': {'class': 'text-end'},
-            'td': {'class': 'text-muted text-end'}
-        }
     )
     actions = tables.TemplateColumn(
         template_name="lot_actions.html",
         verbose_name=_(""),
-        attrs={
-            'th': {'class': 'text-end'},
-            'td': {'class': 'text-end'}
-        }
     )
 
     def render_archived(self, value):
@@ -81,10 +56,7 @@ class LotTable(tables.Table):
         return mark_safe('<span class="badge bg-success"><i class="bi bi-folder-fill"></i></span>')
 
     class Meta:
+        template_name = "custom_table.html"
         model = Lot
         fields = ("select", "archived", "name", "description", "device_count", "created", "user", "actions")
-        attrs = {
-            "class": "table table-hover align-middle",
-            "thead": {"class": "table-light"}
-        }
         order_by = ("-created",)
