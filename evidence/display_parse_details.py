a947ce5366f0557b080c5d82ba7b5fad91acf034
Thomas Rusiecki <rusieckithomas@gmail.com>
2025-10-24 00:00:45 -0300

    move all display parsing to details file

--- evidence/display_parse_details.py
+++ evidence/display_parse_details.py
@@ -1,9 +1,9 @@
-import json
 import logging
-
-
+import pandas as pd
 
 logger = logging.getLogger('django')
+
+MANUFACTURER_CSV_PATH = 'utils/pnp_manufacturer_ids.csv'
 
 class ParseSnapshot:
     def __init__(self, snapshot, default="n/a"):
@@ -20,10 +20,11 @@ class ParseSnapshot:
 
         self.device = {
             "actions": [],
+            "manufacturer_id": info.get("Manufacturer ID"),
             "manufacturer": info.get("Manufacturer"),
             "model": info.get("Model"),
             "serialNumber": info.get("Serial Number"),
-            "version": info.get("EDID Version"),
+            "edid_version": info.get("EDID Version"),
         }
 
         #done this way so it rendes properly on components tab
@@ -64,8 +65,20 @@ class ParseSnapshot:
         base = edid_dict.get("Block 0, Base EDID", {})
         native = edid_dict.get("Native Video Resolution", {})
 
+        manufacturer_map = {}
+        try:
+            manufacturer_df = pd.read_csv(MANUFACTURER_CSV_PATH)
+            manufacturer_map = manufacturer_df.set_index('PNP ID')['Company'].to_dict()
+        except FileNotFoundError:
+            logger.warning(f"Manufacturer mapping file not found at {MANUFACTURER_CSV_PATH}. Using raw IDs.")
+        except Exception as e:
+            logger.error(f"Error loading manufacturer mapping CSV: {e}")
+
+        man_string = manufacturer_map.get(base.get("Manufacturer",""))
+
         info = {
-            "Manufacturer": base.get("Manufacturer"),
+            "Manufacturer": man_string,
+            "Manufacturer ID": base.get("Manufacturer"),
             "EDID Version": base.get("EDID Structure Version & Revision"),
             "Serial Number": base.get("Serial Number"),
             "Model": base.get("Model"),
@@ -75,6 +88,7 @@ class ParseSnapshot:
             "Color Format": base.get("Supported color formats"),
             "Native Resolution": (native.get("_misc") or [None])[0],
             "Preferred Timing": base.get("DTD 1"),
+            "Device Type": "Display",
         }
 
         misc = base.get("_misc")
