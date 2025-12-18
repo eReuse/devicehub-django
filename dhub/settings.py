ab5c2df35c4f7409292d9c22a75f392b82495d15
cayo <cayo@puigdefabregas.eu>
2025-12-18 12:18:37 +0100

    add htmlx flow

--- dhub/settings.py
+++ dhub/settings.py
@@ -90,6 +90,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     'django.middleware.locale.LocaleMiddleware',
     "django.middleware.csrf.CsrfViewMiddleware",
+    "django_htmx.middleware.HtmxMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
