fa30fe825fa077f9830c2cae4fada9d4a862c799
Sergio Giménez <hi@sergiogimenez.com>
2025-12-23 16:55:59 +0100

    Add Feature: Environmental Impact per Lot/Batch
    Brief extract of some of the commits:
    
    - Added environmental impact as a django app
    - Added carbon intensity package
    - Added algorithm ereuse25
    - Implement logic to extract total_usage_time and reuse_time
    - Implement disk change function
    - Added reuse time summary
    - We only support three types now: laptop, desktop and unknown
    - Get time in oldest chronological order and improve readibility
    - Added unit tests for env impact per lot
    - If power-on-hours is 0, consider invalid data
    - Added models to admin interface
    - Added new migrations related to environmental impact models
    - Added docs with mathjax support for formulas
    - If impact is not there, then show a message explaining that the data
      is invalid for several reasons
    - Also consider sleep mode

--- environmental_impact/admin.py
+++ environmental_impact/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from environmental_impact.models import (
+    DeviceLifecycleMetrics,
+    DiskChangeEvent
+)
 
-# Register your models here.
+
+@admin.register(DeviceLifecycleMetrics)
+class DeviceLifecycleMetricsAdmin(admin.ModelAdmin):
+    list_display = [
+        'device_chid',
+        'total_usage_time',
+        'reuse_time',
+        'evidence_count',
+        'disk_change_count',
+        'updated'
+    ]
+    search_fields = ['device_chid']
+    list_filter = ['owner', 'updated', 'disk_change_count']
+    readonly_fields = ['created', 'updated']
+
+
+@admin.register(DiskChangeEvent)
+class DiskChangeEventAdmin(admin.ModelAdmin):
+    list_display = [
+        'device_chid',
+        'evidence_index',
+        'old_disk_model',
+        'new_disk_model',
+        'poh_before_change',
+        'poh_after_change',
+        'created'
+    ]
+    search_fields = ['device_chid', 'evidence_uuid']
+    list_filter = ['owner', 'created']
+    readonly_fields = ['created', 'updated']
