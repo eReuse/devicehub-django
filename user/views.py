711c1c6f72c28904ee2713b298050d44e88ef1d9
selankon <selankon@selankon.xyz>
2026-01-12 13:09:26 +0100

    Fix missing import

--- user/views.py
+++ user/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils.translation import gettext_lazy as _
 from django.views.generic.base import TemplateView
-from dashboard.mixins import DashboardView
+from dashboard.mixins import DashboardView, Http403
 from django.views.generic.edit import (
     FormView,
 )
@@ -25,7 +25,7 @@ class PanelView(DashboardView, TemplateView):
     breadcrumb = "User / Panel"
     subtitle = "User panel"
 
-    
+
 class SettingsView(DashboardView, FormView):
     template_name = "settings.html"
     title = _("Download Settings")
@@ -37,17 +37,17 @@ class SettingsView(DashboardView, FormView):
         settings_tmpl = "settings.ini"
         path = reverse("api:new_snapshot")
         cleaned_data['url'] = self.request.build_absolute_uri(path)
-        
+
         if config("LEGACY", False):
             cleaned_data['token'] = config.get('TOKEN_LEGACY', '')
             cleaned_data['url'] = config.get('URL_LEGACY', '')
             settings_tmpl = "settings_legacy.ini"
-            
+
         data = render(self.request, settings_tmpl, cleaned_data)
         response = HttpResponse(data.content, content_type="application/text")
         response['Content-Disposition'] = 'attachment; filename={}'.format("settings.ini")
         return response
-    
+
     def get_form_kwargs(self):
         tokens = Token.objects.filter(owner=self.request.user)
         kwargs = super().get_form_kwargs()
