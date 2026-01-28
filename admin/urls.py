699546c7e1ecfe2bcff3a2a3152cd15d05e647a4
Thomas Rusiecki <rusieckithomas@gmail.com>
2026-01-28 02:29:29 -0300

    implement dfr

--- admin/urls.py
+++ admin/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path("users/delete/<int:pk>", views.DeleteUserView.as_view(), name="delete_user"),
     path("institution/<int:pk>", views.InstitutionView.as_view(), name="institution"),
     path("institution/<int:pk>/settings", views.InstitutionConfigView.as_view(), name="institution_settings"),
+    path("institution/<int:pk>/dfr/issue", views.IssueDigitalFacilityRecordView.as_view(), name="institution_dfr_issue"),
+
     path("states/", views.StatesPanelView.as_view(), name="states_panel"),
     path("states/add", views.AddStateDefinitionView.as_view(), name="add_state_definition"),
     path("states/delete/<int:pk>", views.DeleteStateDefinitionView.as_view(), name='delete_state_definition'),
