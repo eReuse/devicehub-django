04e12df83414d9fc35aeccc52ffc25daefca4b7d
selankon <selankon@selankon.xyz>
2026-02-04 09:42:31 +0100

    Improve information sent to beneficiary view

--- lot/views.py
+++ lot/views.py
@@ -774,19 +774,37 @@ class BeneficiaryView(DashboardLotMixing, BeneficiaryAgreementEmail, FormView):
             else:
                 beneficiaries = Beneficiary.objects.filter(lot=self.lot)
 
-        new_devices = {}
-        for d in self.request.session.get("devices", []):
+        devices_to_assign = []
+        devices_already_assigned = []
+        for device_id in self.request.session.get("devices", []):
+            device = Device(id=device_id, owner=self.request.user.institution)
+            device.initial()
+
             d_ben = DeviceBeneficiary.objects.filter(
-                device_id=d,
+                device_id=device_id,
                 beneficiary__lot=self.lot
             ).first()
-            new_devices[d] = d_ben.beneficiary.email if d_ben else ''
+
+            device_info = {
+                'id': device_id,
+                'shortid': device.shortid,
+                'type': device.type or '',
+                'manufacturer': device.manufacturer or '',
+                'model': device.model or '',
+            }
+
+            if d_ben:
+                device_info['beneficiary_email'] = d_ben.beneficiary.email
+                devices_already_assigned.append(device_info)
+            else:
+                devices_to_assign.append(device_info)
 
         context.update({
             'lot': self.lot,
             'beneficiaries': beneficiaries,
             "action": _("Add"),
-            "new_devices": new_devices.items()
+            "devices_to_assign": devices_to_assign,
+            "devices_already_assigned": devices_already_assigned,
         })
         return context
 
