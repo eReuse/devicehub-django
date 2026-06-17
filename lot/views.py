f931a794f5990c07d127f1b05d3c373de85e4fef
selankon <selankon@selankon.xyz>
2026-06-17 15:28:08 +0200

    Fix https://farga.pangea.org/ereuse/projectes/issues/740

--- lot/views.py
+++ lot/views.py
@@ -861,8 +861,9 @@ class BeneficiaryView(DashboardLotMixing, BeneficiaryInterestedEmail, FormView):
             device = Device(id=device_id, owner=self.request.user.institution)
             device.initial()
 
+            aliases = RootAlias.physical_aliases(self.request.user.institution, device_id)
             d_ben = DeviceBeneficiary.objects.filter(
-                device_id=device_id,
+                device_id__in=aliases,
                 beneficiary__lot=self.lot
             ).exclude(status=DeviceBeneficiary.Status.RETURNED).first()
 
